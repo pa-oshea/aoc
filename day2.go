b4e27c62fc439c4c416499253053612e25a8a4bd
Patrick O'Shea <p14oshea@gmail.com>
2022-12-06 18:54:33 +0000

    Day 2

---
+++ day2.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"strings"
+)
+
+// Win lose draw
+const (
+	W = 6
+	L = 0
+	D = 3
+)
+
+var matcher = map[string]int{
+	"X": 1, // rock
+	"Y": 2, // paper
+	"Z": 3, // scissors
+	"A": 1,
+	"B": 2,
+	"C": 3,
+}
+
+func day2_part1() (r int) {
+	str := readFile("day2.txt")
+	for _, el := range str {
+		spl := strings.Split(el, " ")
+		player := spl[1]
+		opponent := spl[0]
+
+		r += matcher[player]
+
+		if matcher[player]-matcher[opponent] == 1 || matcher[opponent]-matcher[player] == 2 {
+			r += W
+		} else if matcher[opponent]-matcher[player] == 1 || matcher[player]-matcher[opponent] == 2 {
+			r += L
+		} else {
+			r += D
+		}
+	}
+	return
+}
+
+func day2_part2() (r int) {
+	str := readFile("day2.txt")
+	for _, el := range str {
+		spl := strings.Split(el, " ")
+		player := spl[1]
+		opponent := spl[0]
+
+		if player == "Y" {
+			// draw
+			r += D + matcher[opponent]
+		} else if player == "X" {
+			// lose
+			r += L
+			if matcher[opponent]-1 > 0 {
+				r += matcher[opponent] - 1
+			} else {
+				r += 3
+			}
+		} else {
+			// win
+			r += W
+			if matcher[opponent]+1 > 3 {
+				r += 1
+			} else {
+				r += matcher[opponent] + 1
+			}
+		}
+	}
+	return
+}
--- main.go
+++ main.go
@@ -6,5 +6,7 @@ import (
 
 func main() {
 	fmt.Println(fmt.Sprintf("Day 1 part 1: %d", day1_part1()))
-    fmt.Println(fmt.Sprintf("Day 1 part 2: %d", day1_part2()))
+	fmt.Println(fmt.Sprintf("Day 1 part 2: %d", day1_part2()))
+	fmt.Println(fmt.Sprintf("Day 2 part 1: %d", day2_part1()))
+	fmt.Println(fmt.Sprintf("Day 2 part 1: %d", day2_part2()))
 }
